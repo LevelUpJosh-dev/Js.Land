474cc56ac0250b232ada52ab796a17c46af569fc
Joshua Rei <xxstubzxx@gmail.com>
2022-07-02 19:35:39 -0400

    Image Dump
    Additional Styling and copy updates.

--- pages/home/home.js
+++ pages/home/home.js
@@ -9,7 +9,7 @@ async function Home () {
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="" />
                 <meta name="author" content="" />
-                <title>Grayscale - Start Bootstrap Theme</title>
+                <title>Js.Land</title>
                 <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
                 <style>
                     ${await Serve(`Bootstrap`)}
@@ -27,7 +27,7 @@ async function Home () {
                 <!-- Navigation-->
                 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
                     <div class="container px-4 px-lg-5">
-                        <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                        <a class="navbar-brand" href="#page-top">Js.Land</a>
                         <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                             Menu
                             <i class="fas fa-bars"></i>
@@ -46,9 +46,9 @@ async function Home () {
                     <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                         <div class="d-flex justify-content-center">
                             <div class="text-center">
-                                <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
-                                <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-                                <a class="btn btn-primary" href="#about">Get Started</a>
+                                <h1 class="mx-auto">Js.Land</h1>
+                                <h2 class="text-white-50 mx-auto mt-2 mb-5">Your source for everything code</h2>
+                                <a class="btn btn-primary" href="#about">Learn more..</a>
                             </div>
                         </div>
                     </div>
@@ -57,16 +57,18 @@ async function Home () {
                 <section class="about-section text-center" id="about">
                     <div class="container px-4 px-lg-5">
                         <div class="row gx-4 gx-lg-5 justify-content-center">
-                            <div class="col-lg-8">
-                                <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
+                            <div class="col-lg-6 mb-5">
+                                <img class="img-fluid" src="assets/img/coffee-code.png" alt="..." />
+                            </div>
+                            <div class="col-lg-6 mt-5">
+                                <h3 class="text-white mb-4">Hi my name is Josh, I'm a Full-Stack developer and general Js enthusiast.</h3>
                                 <p class="text-white-50">
-                                    Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                                    <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
-                                    The theme is open source, and you can use it for any purpose, personal or commercial.
-                                </p>
+                                    My aim with Js.Land is to be an informative space for development news and info for both professional and aspiring web developers. 
+                                    I believe the current landscape of technology and development specifically the ability of anyone to self-teach is unprecedented.
+                                    Being a self-taught developer myself, I felt I could contribute to the general development community in positive way.
+                                 </p>
                             </div>
                         </div>
-                        <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
                     </div>
                 </section>
                 <!-- Projects-->
@@ -74,22 +76,26 @@ async function Home () {
                     <div class="container px-4 px-lg-5">
                         <!-- Featured Project Row-->
                         <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                            <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
+                            <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0 border-0" src="assets/img//home/cloud-code.png" alt="..." /></div>
                             <div class="col-xl-4 col-lg-5">
                                 <div class="featured-text text-center text-lg-left">
-                                    <h4>Shoreline</h4>
-                                    <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                                    <h4>Knowledge</h4>
+                                    <p class="text-black-50 mb-0">
+                                        Weather it be YouTube, Twitter, Free Code Academy or some other resource
+                                        There is truely a wealth of knowledge and information available to you.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                         <!-- Project One Row-->
                         <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                            <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+                            <div class="col-lg-6">
+                                <img class="img-fluid" src="assets/img/home/scrabble-dev.jpg" alt="..." />
+                            </div>
                             <div class="col-lg-6">
                                 <div class="bg-black text-center h-100 project">
                                     <div class="d-flex h-100">
                                         <div class="project-text w-100 my-auto text-center text-lg-left">
-                                            <h4 class="text-white">Misty</h4>
                                             <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
                                             <hr class="d-none d-lg-block mb-0 ms-0" />
                                         </div>
@@ -99,12 +105,11 @@ async function Home () {
                         </div>
                         <!-- Project Two Row-->
                         <div class="row gx-0 justify-content-center">
-                            <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+                            <div class="col-lg-6"><img class="img-fluid" src="assets/img//home/scrabble-js.jpg" alt="..." /></div>
                             <div class="col-lg-6 order-lg-first">
                                 <div class="bg-black text-center h-100 project">
                                     <div class="d-flex h-100">
                                         <div class="project-text w-100 my-auto text-center text-lg-right">
-                                            <h4 class="text-white">Mountains</h4>
                                             <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
                                             <hr class="d-none d-lg-block mb-0 me-0" />
                                         </div>
@@ -166,7 +171,7 @@ async function Home () {
                                 <div class="card py-4 h-100">
                                     <div class="card-body text-center">
                                         <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                                        <h4 class="text-uppercase m-0">Address</h4>
+                                        <h4 class="text-uppercase m-0">Youtube</h4>
                                         <hr class="my-4 mx-auto" />
                                         <div class="small text-black-50">4923 Market Street, Orlando FL</div>
                                     </div>
@@ -178,7 +183,7 @@ async function Home () {
                                         <i class="fas fa-envelope text-primary mb-2"></i>
                                         <h4 class="text-uppercase m-0">Email</h4>
                                         <hr class="my-4 mx-auto" />
-                                        <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                                        <div class="small text-black-50"><a href="#!">josh@js.land.com</a></div>
                                     </div>
                                 </div>
                             </div>
@@ -186,22 +191,17 @@ async function Home () {
                                 <div class="card py-4 h-100">
                                     <div class="card-body text-center">
                                         <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                                        <h4 class="text-uppercase m-0">Phone</h4>
+                                        <h4 class="text-uppercase m-0">Twitter</h4>
                                         <hr class="my-4 mx-auto" />
-                                        <div class="small text-black-50">+1 (555) 902-8832</div>
+                                        <div class="small text-black-50"></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="social d-flex justify-content-center">
-                            <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                            <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
-                        </div>
                     </div>
                 </section>
                 <!-- Footer-->
-                <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2022</div></footer>
+                <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Js.Land 2022</div></footer>
                 <!-- Bootstrap core JS-->
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
             </body>
