ed84c559a488244a51dd218fb48d5e9efc4e8eef
Joshua Rei <xxstubzxx@gmail.com>
2022-07-02 15:47:43 -0400

    Update home page
    Remove remote fonts
    remove extra sugar remote resources.
    Bring bootstrap into local and parse server-side directly into the template.
    Template custom bootstrap Js

--- lobot.js
+++ lobot.js
@@ -1,12 +1,25 @@
 
 async function Sherlock () {
-    const Sherlock = await Deno.readTextFile("./helpers/Sherlock.js");
-    return Sherlock;
+    return await Deno.readTextFile("./helpers/Sherlock.js");
+}
+
+async function BootstrapCustomJs () {
+    return await Deno.readTextFile("./helpers/BootstrapCustom.js");
+}
+
+async function Bootstrap () {
+    return await Deno.readTextFile("./helpers/Bootstrap.css");
 }
 
 async function Serve (resource) {
     if (resource === "Sherlock") {
         return await Sherlock();
+    }
+    if (resource === "Bootstrap") {
+        return await Bootstrap();
+    }
+    if (resource === "BootstrapCustomJs") {
+        return await BootstrapCustomJs();
     }
 }
 
