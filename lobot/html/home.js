705359e52cab376b63a0bf46f0f08376c75e8917
Joshua Rei <xxstubzxx@gmail.com>
2022-07-08 22:25:58 -0400

    Adds Fragments
    Adds content grid and navigation fragments

--- lobot/html/home.js
+++ lobot/html/home.js
@@ -2,30 +2,14 @@ import { Serve } from "../lobot.js";
 
 async function Home() {
   const Head = await Serve(`Head:html`);
+  const Navigation = await Serve(`Navigation:html`);
   const Footer = await Serve(`Footer:html`);
   return `
         <!DOCTYPE html>
         <html lang="en">
             ${await Head()}
             <body id="page-top">
-                <!-- Navigation-->
-                <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-                    <div class="container px-4 px-lg-5">
-                        <a class="navbar-brand" href="#page-top" aria-label="Js.Land main navigation">
-                            <img width="485px" height="485px" src="assets/img/home/webp/logo-solid.webp" alt="Js.Land logo image link" class="img-fluid logo-solid pb-3" />
-                        </a>
-                        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                            Menu
-                            <i class="fas fa-bars"></i>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarResponsive">
-                            <ul class="navbar-nav ms-auto">
-                                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                ${await Navigation()}
                 <!-- Masthead-->
                 <header class="masthead">
                     <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
@@ -205,7 +189,6 @@ async function Home() {
 }
 
 export { Home };
-
 
 /**
  * TODO: ID binding from html fragments then allow front end to make basic call to server route for the given bound Id to the fragment
--- lobot/lobot.js
+++ lobot/lobot.js
@@ -1,35 +1,36 @@
-import { Head, Footer } from "./fragments.js";
+import { ContentGrid, Footer, Head, Navigation } from "./fragments.js";
 
 const fragmentBundle = {
-    Head: Head,
-    Footer: Footer
-}
+  ContentGrid: ContentGrid,
+  Head: Head,
+  Footer: Footer,
+  Navigation: Navigation,
+};
 
 /** Reads and returns the text content of the given resource **/
-async function LoadResource (name, type) {
+async function LoadResource(name, type) {
   const lobotResourcePath = `lobot/${type}/${name}.${type}`;
 
   return (type === `css`)
-          ? `<style>${await Deno.readTextFile(lobotResourcePath)}</style>`
-          : (type === `js`)
-          ? `<script>${await Deno.readTextFile(lobotResourcePath)}</script>`
-          : (type === `txt`)
-          ? `<p>${await Deno.readTextFile(lobotResourcePath)}</p>`
-          : (type === `json`)
-          ? await Deno.readTextFile(lobotResourcePath)
-          : null;
+    ? `<style>${await Deno.readTextFile(lobotResourcePath)}</style>`
+    : (type === `js`)
+    ? `<script>${await Deno.readTextFile(lobotResourcePath)}</script>`
+    : (type === `txt`)
+    ? `<p>${await Deno.readTextFile(lobotResourcePath)}</p>`
+    : (type === `json`)
+    ? await Deno.readTextFile(lobotResourcePath)
+    : null;
 }
 
 async function Serve(resource) {
   const name = resource.split(`:`)[0];
   const type = resource.split(`:`)[1];
-  if (type === `css` || type === `js` || type === `txt` ) {
+  if (type === `css` || type === `js` || type === `txt`) {
     return await LoadResource(name, type);
   }
   if (type === `html`) {
     return await fragmentBundle[name];
   }
 }
-
 
 export { Serve };
